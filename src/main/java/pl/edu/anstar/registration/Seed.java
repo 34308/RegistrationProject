8e0c500d3ce2a31f7f8ac6b33e5f3c1cb1247459
broniuu <86781217+broniuu@users.noreply.github.com>
2024-01-04 12:37:06 +0100

    Add verification (#2)
    * Add login to dto
    
    * Add verification
    
    ---------
    
    Co-authored-by: broni <broniekf@gmail,com>

---
+++ src/main/java/pl/edu/anstar/registration/Seed.java
@@ -0,0 +1,48 @@
+package pl.edu.anstar.registration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import pl.edu.anstar.registration.model.User;
+import pl.edu.anstar.registration.repository.UserRepository;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class Seed implements CommandLineRunner {
+    private final UserRepository userRepository;
+
+    @Override
+    public void run(String... args) {
+        seedData();
+    }
+
+    private void seedData() {
+        if (userRepository.count() > 0) {
+            return;
+        }
+        List<User> users = List.of(
+                User.builder()
+                        .email("andrzej.duda@gmail.com")
+                        .name("Andrzej")
+                        .surname("Duda")
+                        .password("prezydent123")
+                        .build(),
+                User.builder()
+                        .email("donald.tusk@gmail.com")
+                        .name("Donald")
+                        .surname("Tusk")
+                        .password("premier123")
+                        .build(),
+                User.builder()
+                        .email("kazimierz.tetmeier@gmail.com")
+                        .name("Kazimierz")
+                        .surname("Tetmeier")
+                        .password("przerwa123")
+                        .build()
+        );
+        userRepository.saveAll(users);
+    }
+}
--- src/main/java/pl/edu/anstar/registration/worker/VerifyDataWorker.java
+++ src/main/java/pl/edu/anstar/registration/worker/VerifyDataWorker.java
@@ -3,22 +3,36 @@ package pl.edu.anstar.registration.worker;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import pl.edu.anstar.registration.Services.UserService;
+import pl.edu.anstar.registration.dto.UserDto;
 
 @Component
+@RequiredArgsConstructor
 public class VerifyDataWorker {
     private static final Logger LOGGER = LoggerFactory.getLogger(VerifyDataWorker.class);
+    private final UserService userService;
 
     @JobWorker(type = "verifyData")
     public void verifyData(final JobClient client, final ActivatedJob job) {
-        try {
-
-            Thread.sleep(1000); // Sleep for 10 seconds
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        UserDto userDto = job.getVariablesAsType(UserDto.class);
+        boolean userWithEmailExists = userService.userWithThisEmailExists(userDto.getEmail());
+        if (userWithEmailExists){
+            handleError(client, job, "VERIFICATION_FAILED", "User with this email already exists");
         }
         LOGGER.info("verify data");
+    }
+
+    private void handleError(JobClient client, ActivatedJob job, String errorCode, String errorMessage) {
+        LOGGER.error(errorCode + "," + errorMessage);
+
+        client.newThrowErrorCommand(job.getKey())
+                .errorCode(errorCode)
+                .errorMessage(errorMessage)
+                .send()
+                .join();
     }
 }
